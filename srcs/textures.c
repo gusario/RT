638b609af784c4bad7749c54451adbf3a427affe
lminta <lminta@student.21-school.ru>
2019-10-11 23:04:43 +0300

    еще один коммит

--- srcs/textures.c
+++ srcs/textures.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   textures.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lminta <lminta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 15:55:58 by sbrella           #+#    #+#             */
-/*   Updated: 2019/10/11 16:44:21 by jblack-b         ###   ########.fr       */
+/*   Updated: 2019/10/11 23:04:07 by lminta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,18 @@ void	get_texture(char *name, t_txture *texture)
 
 	m = ft_strjoin("./textures/", name);
 	an_surf = IMG_Load(m);
+	printf("%d, %s\n", an_surf, m);
 	ft_strdel(&m);
 	fmt = malloc(sizeof(SDL_PixelFormat));
 	ft_memcpy(fmt, an_surf->format, sizeof(SDL_PixelFormat));
 	fmt->BytesPerPixel = 4;
-	fmt->BitsPerPixel = 32;
-	fmt->Rmask = RMASK;
-	fmt->Gmask = GMASK;
-	fmt->Bmask = BMASK;
-	fmt->Amask = AMASK;
+	fmt->BitsPerPixel = 32;	//			/\
+							//			||
+	fmt->Rmask = RMASK;		//			||
+	fmt->Gmask = GMASK;		//<=====какой-то лютый говнокод
+							//			||
+	fmt->Bmask = BMASK;		//			||
+	fmt->Amask = AMASK;		//			\/
 	surf = SDL_ConvertSurface(an_surf, fmt, an_surf->flags);
 	SDL_FreeSurface(an_surf);
 	free(fmt);
